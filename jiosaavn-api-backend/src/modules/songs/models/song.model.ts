58c7fd4768e747807bde5e7cc5f8073930b116df
Developer <user@example.com>
2026-09-25 14:50:35 +0530

    fix: resolve circular dependency in artist-album model

--- jiosaavn-api-backend/src/modules/songs/models/song.model.ts
+++ jiosaavn-api-backend/src/modules/songs/models/song.model.ts
@@ -1,5 +1,5 @@
 import { DownloadLinkModel } from '#common/models'
-import { ArtistMapAPIResponseModel, ArtistMapModel } from '#modules/artists/models'
+import { ArtistMapAPIResponseModel, ArtistMapModel } from '#modules/artists/models/artist-map.model'
 import { z } from 'zod'
 
 export const SongAPIResponseModel = z.object({
